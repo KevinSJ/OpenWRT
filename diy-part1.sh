713d47775d7414839860ae7428bfd4a435f06a98
Kevin Jiang <KevinSJ@users.noreply.github.com>
2023-01-08 21:51:06 +1300

    [config] Added custom feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git argontheme https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
+echo 'src-git immortalwrt https://github.com/immortalwrt/packages' >>feeds.conf.default
 
 # Use last stable release that's not rc
 git checkout $(git tag -l|grep -v 'rc'|tail -1)
