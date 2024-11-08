570a7d09332546dea2605b70805d4b7a01870ec6
Kevin Jiang <KevinSJ@users.noreply.github.com>
2024-11-08 19:04:59 +1300

    update image builder script

--- image-builder_build.sh
+++ image-builder_build.sh
@@ -2,15 +2,14 @@
 #
 
 PROFILE="linksys_wrt1200ac"
-TO_INSTALL="-dnsmasq -odhcpd-ipv6only base-files bash block-mount busybox ca-bundle coremark curl \
+BASE_PACKAGES="-dnsmasq -odhcpd-ipv6only base-files bash block-mount busybox ca-bundle coremark curl \
 dnsmasq-full dropbear ds-lite e2fsprogs fstools htop iwinfo \
-kmod-gpio-button-hotplug kmod-ipt-nat kmod-ipt-nat6 kmod-lib-zstd kmod-mwlwifi \
-kmod-tcp-bbr libc libgcc libustream-mbedtls logd \
+libc libgcc libustream-openssl logd \
+losetup swap-utils \
 mwlwifi-firmware-88w8864 netifd odhcpd \
 odhcp6c openssh-sftp-server opkg ppp ppp-mod-pppoe procd \
 procd-seccomp resolveip swconfig uboot-envtools uci uclient-fetch urandom-seed \
 urngd wget-ssl -wpad-basic-mbedtls zram-swap aria2 mkf2fs \
-mkf2fs kmod-ppp kmod-pppoe kmod-tun kmod-usb-ohci kmod-usb-uhci kmod-fs-btrfs kmod-fs-ext4 \
 vim-runtime vim-full unbound-control unbound-daemon \
 luci-proto-ipv6 luci-proto-ppp \
 luci-theme-bootstrap luci-theme-openwrt luci-theme-openwrt-2020 luci-theme-material \
@@ -23,7 +22,16 @@ luci-mod-dashboard luci-mod-admin-full \
 luci-app-hd-idle luci-app-ttyd luci-app-wifischedule \
 luci-app-watchcat"
 
-#luci-theme-edge luci-theme-design luci-theme-alpha luci-app-argon-config
+KMOD_PACKAGES="kmod-gpio-button-hotplug kmod-ipt-nat kmod-ipt-nat6  kmod-mwlwifi kmod-loop \
+kmod-ppp kmod-pppoe kmod-tun kmod-usb-ohci kmod-usb-uhci kmod-fs-ext4"
+
+# Add KMOD packages if KMOD_INCLUDED flag is set
+if [ "$KMOD_INCLUDED" = "1" ]; then
+    TO_INSTALL="$BASE_PACKAGES $KMOD_PACKAGES"
+else
+    TO_INSTALL="$BASE_PACKAGES"
+fi
 
 make manifest PACKAGES="$TO_INSTALL" PROFILE=$PROFILE
 make image PACKAGES="$TO_INSTALL" PROFILE=$PROFILE
+
