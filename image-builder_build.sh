11dcab7e0c13a6df1186e4dd99f13bc2c734d1bb
Kevin Jiang <KevinSJ@users.noreply.github.com>
2025-01-11 23:28:55 +1300

    Update image builder script

--- image-builder_build.sh
+++ image-builder_build.sh
@@ -3,35 +3,27 @@
 
 PROFILE="linksys_wrt1200ac"
 BASE_PACKAGES="-dnsmasq -odhcpd-ipv6only base-files bash block-mount busybox ca-bundle coremark curl \
-dnsmasq-full dropbear ds-lite e2fsprogs fstools htop iwinfo \
+-dnsmasq-full dropbear ds-lite e2fsprogs fstools htop iwinfo \
 libc libgcc -libustream-openssl logd \
 losetup swap-utils \
 mwlwifi-firmware-88w8864 netifd odhcpd \
 odhcp6c openssh-sftp-server opkg ppp ppp-mod-pppoe procd \
 procd-seccomp resolveip swconfig uboot-envtools uci uclient-fetch urandom-seed \
 urngd wget-ssl -wpad-basic-mbedtls wpad-openssl zram-swap aria2 mkf2fs \
-vim-runtime vim-full unbound-control unbound-daemon knot-dig knot-libs \
-luci-proto-ipv6 luci-proto-ppp \
-luci-theme-bootstrap luci-theme-openwrt luci-theme-openwrt-2020 luci-theme-material \
-luci-app-firewall luci-app-opkg luci-base luci-compat luci-lib-ipkg mtd \
-luci-app-adblock luci-app-advanced-reboot \
-luci-app-banip luci-app-commands \
-luci-app-ttyd luci-app-vnstat2 luci-app-unbound \
-luci-app-uhttpd \
-luci-mod-dashboard luci-mod-admin-full \
-luci-app-hd-idle luci-app-ttyd luci-app-wifischedule \
-luci-app-watchcat"
+vim-runtime vim-full unbound-control unbound-daemon knot-dig knot-libs tcpdump msmtp \
+luci-proto-ipv6 luci-proto-ppp luci-proto-nebula nebula nebula-proto \
+luci-theme-argon luci-theme-bootstrap luci-theme-openwrt luci-theme-openwrt-2020 luci-theme-material \
+luci-app-firewall luci-app-opkg luci-base luci-compat luci-lib-ip luci-lib-jsonc luci-lib-ipkg mtd \
+luci-app-adblock luci-app-advanced-reboot luci-app-uhttpd \
+luci-app-banip luci-app-vnstat2 luci-app-unbound luci-app-nlbwmon \
+luci-mod-dashboard luci-mod-network luci-mod-system luci-mod-status luci-mod-admin-full luci-app-wifischedule"
 
 KMOD_PACKAGES="kmod-gpio-button-hotplug kmod-ipt-nat kmod-ipt-nat6  kmod-mwlwifi kmod-loop \
 kmod-ppp kmod-pppoe kmod-tun kmod-usb-ohci kmod-usb-uhci kmod-fs-ext4"
 
 # Add KMOD packages if KMOD_INCLUDED flag is set
-if [ "$KMOD_INCLUDED" = "1" ]; then
-    TO_INSTALL="$BASE_PACKAGES $KMOD_PACKAGES"
-else
-    TO_INSTALL="$BASE_PACKAGES"
-fi
+TO_INSTALL="$BASE_PACKAGES $KMOD_PACKAGES"
 
-make manifest PACKAGES="$TO_INSTALL" PROFILE=$PROFILE
-make image PACKAGES="$TO_INSTALL" PROFILE=$PROFILE
+make -j$(nproc) manifest PACKAGES="$TO_INSTALL" PROFILE=$PROFILE
+make -j$(nproc) image PACKAGES="$TO_INSTALL" PROFILE=$PROFILE
 
