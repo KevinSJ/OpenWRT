fe954cb49164d6ac2425798951849e48a5a41d9a
Kevin Sicong Jiang <KevinSJ@users.noreply.github.com>
2025-04-18 14:59:11 +1200

    Update image-builder_build.sh
    Remove luci-theme-argon from image builder

--- image-builder_build.sh
+++ image-builder_build.sh
@@ -12,12 +12,12 @@ procd-seccomp resolveip swconfig uboot-envtools uci uclient-fetch urandom-seed \
 urngd wget-ssl -wpad-basic-mbedtls wpad-openssl zram-swap aria2 mkf2fs \
 vim-runtime vim-full unbound-control unbound-daemon knot-dig knot-libs tcpdump msmtp \
 luci-proto-ipv6 luci-proto-ppp luci-proto-nebula nebula nebula-proto \
-luci-theme-argon luci-theme-bootstrap luci-theme-openwrt luci-theme-openwrt-2020 luci-theme-material \
+luci-theme-bootstrap luci-theme-openwrt luci-theme-openwrt-2020 luci-theme-material \
 luci-app-firewall luci-app-opkg luci-base luci-compat luci-lib-ip luci-lib-jsonc luci-lib-ipkg mtd \
 luci-app-adblock luci-app-advanced-reboot luci-app-uhttpd \
 luci-app-banip luci-app-vnstat2 luci-app-unbound luci-app-nlbwmon \
 luci-mod-dashboard luci-mod-network luci-mod-system luci-mod-status luci-mod-admin-full luci-app-wifischedule"
-
+# luci-theme-argon
 KMOD_PACKAGES="kmod-gpio-button-hotplug kmod-ipt-nat kmod-ipt-nat6  kmod-mwlwifi kmod-loop \
 kmod-ppp kmod-pppoe kmod-tun kmod-usb-ohci kmod-usb-uhci kmod-fs-ext4"
 
